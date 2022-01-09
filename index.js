a8a0cb9fee5c4a2b1d547c03e0ebc811480dcf66
faisalcsebd <farhana.cse.bd@gmail.com>
2022-01-09 19:06:11 +0600

    services data post and get done

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000;
+
+const cors = require('cors');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+app.use(cors());
+app.use(express.json());
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cu5az.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// console.log(uri);
+async function run() {
+    try {
+        await client.connect()
+        console.log("database connnecteddd");
+        const database = client.db('eventDB');
+        const servicesCollection = database.collection('services');
+
+
+
+
+        // GET API
+        app.get('/services', async (req, res) => {
+            const cursor = servicesCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+
+
+
+        // POST API
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+            console.log('hit the post api', service);
+            const result = await servicesCollection.insertOne(service);
+            console.log(result);
+            res.json(result)
+        });
+
+
+
+
+
+
+    }
+    finally {
+    }
+}
+run().catch(console.dir);
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
